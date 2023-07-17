e3cf548ad7ea3bf205433513db93eda600848c78
mononokai <leachda12@icloud.com>
2023-07-17 16:15:29 -0400

    feat: add grey out for sold out items

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -74,11 +74,18 @@ const Menu = () => {
             <h2>Our Menu</h2>
 
             {numPizzas > 0 ? (
-                <ul className="pizzas">
-                    {pizzas.map((pizza) => (
-                        <Pizza pizzaObj={pizza} key={pizza.name} />
-                    ))}
-                </ul>
+                <>
+                    <p>
+                        Authentic Italian cuisine. 6 creative dishes to choose
+                        from. All from our stone oven, all organic, all
+                        delicious.
+                    </p>
+                    <ul className="pizzas">
+                        {pizzas.map((pizza) => (
+                            <Pizza pizzaObj={pizza} key={pizza.name} />
+                        ))}
+                    </ul>
+                </>
             ) : (
                 <p>We're still working on our menu. Please come back later.</p>
             )}
@@ -92,22 +99,22 @@ const Menu = () => {
     );
 };
 
-const Pizza = (props) => {
-    console.log(props);
+const Pizza = ({ pizzaObj }) => {
+    // if (pizzaObj.soldOut) return null;
 
     return (
-        <li className="pizza">
-            <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
-            <h3>{props.pizzaObj.name}</h3>
-            <p>{props.pizzaObj.ingredients}</p>
-            <span>{props.pizzaObj.price}</span>
+        <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+            <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+            <h3>{pizzaObj.name}</h3>
+            <p>{pizzaObj.ingredients}</p>
+            <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
         </li>
     );
 };
 
 const Footer = () => {
     const hour = new Date().getHours();
-    const openHour = 16;
+    const openHour = 12;
     const closeHour = 22;
     const isOpen = hour >= openHour && hour < closeHour;
 
@@ -124,17 +131,25 @@ const Footer = () => {
     return (
         <footer className="footer">
             {isOpen ? (
-                <div className="order">
-                    <p>
-                        We're open until {closeHour}:00. Come visit us or order
-                        online
-                    </p>
-                    <button className="btn">Order</button>
-                </div>
+                <Order closeHour={closeHour} />
             ) : (
-                <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>
+                <p>
+                    We're happy to welcome you between {openHour}:00 and{' '}
+                    {closeHour}:00.
+                </p>
             )}
         </footer>
+    );
+};
+
+const Order = ({ closeHour }) => {
+    return (
+        <div className="order">
+            <p>
+                We're open until {closeHour}:00. Come visit us or order online
+            </p>
+            <button className="btn">Order</button>
+        </div>
     );
 };
 
