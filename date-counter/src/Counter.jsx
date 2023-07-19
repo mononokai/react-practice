3a6555234ebd5adfed2a338559b1db9ef963e7ad
mononokai <leachda12@icloud.com>
2023-07-19 19:50:51 -0400

    feat: add range slider for steps

--- date-counter/src/Counter.jsx
+++ date-counter/src/Counter.jsx
@@ -6,26 +6,36 @@ export const Counter = () => {
     const day = new Date('July 18 2023');
     day.setDate(day.getDate() + count);
 
-    const handleMinusStep = () => {
-        setStep((s) => s - 1);
-    };
+    // const handleMinusStep = () => {
+    //     setStep((s) => s - 1);
+    // };
 
-    const handlePlusStep = () => {
-        setStep((s) => s + 1);
-    };
+    // const handlePlusStep = () => {
+    //     setStep((s) => s + 1);
+    // };
 
     const handleMinusCount = () => {
         setCount((c) => c - step);
+        console.log(`minus count: ${count}, step: ${step}`)
     };
 
     const handlePlusCount = () => {
         setCount((c) => c + step);
+        console.log(`plus count: ${count}, step: ${step}`)
     };
+
+    const handleRange = (e) => {
+        setStep(Number(e.target.value));
+    }
+
+    const handleInput = (e) => {
+        setCount(Number(e.target.value));
+    }
 
     return (
         <>
             <div>
-                <button onClick={handleMinusStep}>-</button>
+                <input type="range" min='1' max='10' value={step} onChange={e => handleRange(e)}/>
                 <span>Step: {step}</span>
                 <button onClick={handlePlusStep}>+</button>
             </div>
