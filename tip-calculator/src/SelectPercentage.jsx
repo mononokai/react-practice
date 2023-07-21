afd4044d6ed8f662f4e835d2468b51ee6fcaf76b
mononokai <leachda12@icloud.com>
2023-07-20 23:53:40 -0400

    feat: add SelectPercentage component

--- tip-calculator/src/SelectPercentage.jsx
+++ tip-calculator/src/SelectPercentage.jsx
@@ -1,3 +1,13 @@
-export function SelectPercentage() {
-    
+export function SelectPercentage({ children, currentPercent, onPercentChange }) {
+    return (
+        <div>
+            <span>{children}</span>
+            <select>
+                <option value="0">Dissatisfied (0%)</option>
+                <option value="0">It was okay (5%)</option>
+                <option value="0">It was good (10%)</option>
+                <option value="0">Absolutely amazing! (20%)</option>
+            </select>
+        </div>
+    )
 }
